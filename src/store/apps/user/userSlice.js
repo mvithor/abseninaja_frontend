36aa4ef6e61da2d187f32305ff830988d6800232
Vithor <mvithor16@gmail.com>
2025-08-17 05:25:50 +0700

    chore(utils): hapus console log ready prod

--- src/store/apps/user/userSlice.js
+++ src/store/apps/user/userSlice.js
@@ -18,15 +18,12 @@ const userSlice = createSlice({
     setUser(state, action) {
       
       const { name, role, email,  userId, accessToken, deviceId } = action.payload;
-      console.log("Payload dari backend:", action.payload);
       try {
         // Validasi token sebelum menyimpannya
         const decodedToken = jwtDecode(accessToken);
-        console.log("Decoded Token di Frontend:", decodedToken); 
         const currentTime = Math.floor(Date.now() / 1000);
 
         if (decodedToken.exp < currentTime) {
-          console.warn('Access token telah kedaluwarsa. Tidak dapat menyimpan ke state.');
           return;
         }
 
@@ -37,14 +34,11 @@ const userSlice = createSlice({
         state.accessToken = accessToken;
         state.deviceId = deviceId;
         state.isLoggedIn = true;
-
-        console.log('User state berhasil diperbarui:', { name, role, email, userId, deviceId });
       } catch (error) {
         console.error('Error saat decode atau validasi token:', error.message);
       }
     },
     clearUser(state) {
-      console.warn('User state di-reset. Semua data pengguna dihapus.');
       state.name = '';
       state.role = '';
       state.email = '';
